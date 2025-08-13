15f8213579594d15839334d5cfa1e2d409cd54fe
muhshi <muhshi>
2025-08-13 16:22:38 +0700

    benerin routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,13 +13,13 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\View\View
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,19 @@
 use App\Http\Controllers\SurveyController;
 use Illuminate\Support\Facades\Route;
 
-
-// Route::view('/', 'dashboard')->name('dashboard');
-// Route::view('/reports', 'reports')->name('reports');
-// // routes/web.php
-// Route::get('/dash', function () {
-//     return view('dashboard', [
-//         'embedUrl' => \App\Support\WilkerstatMetabase::dashboard(6),
-//     ]);
-// });
-Route::get('/surveys', [SurveyController::class, 'index'])->name('surveys.index');
-
-Route::get('/surveys/category/{category}', [SurveyController::class, 'index'])
-    ->name('surveys.byCategory');
-
-Route::get('/surveys/{survey}/embed', [SurveyController::class, 'embed'])->name('surveys.embed');
-
+// ===== Auth default dari Tablar (cukup sekali) =====
 Auth::routes();
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+// ===== Halaman utama =====
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])
+    ->name('dashboard');
+
+// ===== Kalau mau tetap punya /home, tapi tanpa bentrok nama =====
+Route::get('/home', function () {
+    return redirect()->route('dashboard');
+})->name('home');
+
+// ===== Routes survei =====
+Route::get('/surveys', [SurveyController::class, 'index'])->name('surveys.index');
+Route::get('/surveys/category/{category}', [SurveyController::class, 'index'])->name('surveys.byCategory');
+Route::get('/surveys/{survey}/embed', [SurveyController::class, 'embed'])->name('surveys.embed');
